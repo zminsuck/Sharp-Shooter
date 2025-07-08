3737537488bfd6161a18d3f716f81513b9bca49e
SOS <minmins1591@gmail.com>
2025-07-09 00:19:40 +0900

    Player Shoot Ray 활성화

--- Assets/Project/SK/Weapon.cs
+++ Assets/Project/SK/Weapon.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
     StarterAssetsInputs starterAssetsInputs;
 
+    const string SHOOT_STRING = "Shoot";
     private void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
@@ -19,16 +22,16 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return;
 
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-
-            Debug.Log(hit.collider.name);
-
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
